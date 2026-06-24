032ebe4a8ee7ed4e7a072deb548670c110ed1624
andrewhack <karadimov@gmail.com>
2026-06-24 22:52:20 +0300

    feat(publish): Docker Hub publish script + image-based user compose + Quick Start (androshack)

---
+++ docker/publish.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Tag + push the all-in-one image to Docker Hub. Run after: docker login -u androshack
+# Env overrides: LOCAL (source image), REPO, VERSION
+set -e
+
+LOCAL="${LOCAL:-stremio-libtorrent-server:dev}"
+REPO="${REPO:-androshack/stremio-libtorrent-server}"
+VERSION="${VERSION:-0.1.0}"
+
+docker tag "$LOCAL" "$REPO:$VERSION"
+docker tag "$LOCAL" "$REPO:latest"
+docker push "$REPO:$VERSION"
+docker push "$REPO:latest"
+echo "pushed $REPO:$VERSION and $REPO:latest"
